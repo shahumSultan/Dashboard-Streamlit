fb04ec74e578a516b535323a8c552278385b5014
Shahum <shahum@exarta.com>
2023-07-04 15:17:28 +0500

    pandasai integration

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
-from pandasai import PandasAI  
+from pandasai import PandasAI
+from pandasai.llm.openai import OpenAI
 
 st.set_page_config(page_title="Dashboard", layout='wide', initial_sidebar_state='collapsed')
 
@@ -13,11 +14,11 @@ with st.sidebar:
   
 # Check if a file was uploaded
 if uploaded_file is not None:
-   df = pd.read_csv(uploaded_file, encoding="Latin-1")s
+   df = pd.read_csv(uploaded_file, encoding="Latin-1")
    st.write("Uploaded file:") 
    st.dataframe(df)
 
-from pandasai.llm.openai import OpenAI
+
 llm = OpenAI(api_token="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")
 pandas_ai = PandasAI(llm)
 
