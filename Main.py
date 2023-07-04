b8d4c969e5b85110d51e90f5e7fa346fb0df45f2
Shahum <shahum@exarta.com>
2023-07-04 12:16:57 +0500

    minor changes

---
+++ Main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Dashboard", layout='wide', initial_sidebar_state='collapsed')
+
+st.title("Analytics Dashboard")
+with st.sidebar:
+    st.header("Pages")
